b39fad1fdb58e11302609cda8ea34dd12b88aa15
joaonunes <joaonunes4562@gmail.com>
2023-11-01 17:13:39 -0300

    Tela de Contato

---
+++ app/src/main/java/com/agenda/projetoagenda/ContatoTelas.java
@@ -0,0 +1,14 @@
+package com.agenda.projetoagenda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContatoTelas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contato_telas);
+    }
+}
