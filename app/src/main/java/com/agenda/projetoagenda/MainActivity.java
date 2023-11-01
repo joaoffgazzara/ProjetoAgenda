eaec398f72e5881fc7b516bcc2c1389287a8f11a
joaonunes <joaonunes4562@gmail.com>
2023-11-01 16:42:05 -0300

    Initial commit

---
+++ app/src/main/java/com/agenda/projetoagenda/MainActivity.java
@@ -0,0 +1,28 @@
+package com.agenda.projetoagenda;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button botaoRegistro;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        botaoRegistro = findViewById(R.id.buttonRegistro);
+
+        botaoRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,RegistroTela.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/agenda/projetoagenda/RegistroTela.java
@@ -0,0 +1,14 @@
+package com.agenda.projetoagenda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistroTela extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_tela);
+    }
+}
